37b5406a8cb49f7ca3d91810166cc055f3efc85c
向宗亮 <80002480@sf-express.com>
2018-04-08 18:45:42 +0800

    修改登录用户的有效时间

--- app/middleware/islogin.js
+++ app/middleware/islogin.js
@@ -8,12 +8,12 @@ module.exports = options => {
 			getTime = timestamp.getTime();
 		if(ctx.request.url == '/login'){
 			// 如果已经登陆了在访问登陆页面也跳转到首页
-			// if(ctx.request.method == 'GET' && ctx.session.loginTime != undefined && loginTime + 3600000 > getTime) {
-			// 	ctx.redirect('/');
-			// }
+			if(ctx.request.method == 'GET' && ctx.session.loginTime != undefined && loginTime + 10000 > getTime) {
+				ctx.redirect('/');
+			}
 
 		}else{
-			if(loginTime + 3600000 < getTime || !ctx.session.loginTime || ctx.session.loginTime == undefined){
+			if(loginTime + 10000 < getTime || !ctx.session.loginTime || ctx.session.loginTime == undefined){
 				if(ctx.request.method == 'GET' || ctx.request.method == 'get'){
 					ctx.session = null;
 					ctx.redirect('/login');
